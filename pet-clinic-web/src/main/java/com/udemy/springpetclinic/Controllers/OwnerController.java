86d0c8d89f7cf72ede986ade1d7ed41786e73d19
Mi1L4R <keagan.millar100@gmail.com>
2021-10-18 13:36:24 +0200

    - Added Owners list and updated controller and Owner/Index view

--- pet-clinic-web/src/main/java/com/udemy/springpetclinic/Controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/udemy/springpetclinic/Controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.udemy.springpetclinic.Controllers;
 
+import Services.IOwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners") //Instead of RequestMapping on Method with 'owners' before everything, you can describe the initial route here
 @Controller
 public class OwnerController {
 
+    private final IOwnerService ownerService;
+
+    public OwnerController(IOwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
