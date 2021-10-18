502da9615f91d220edeeff30e900534cb26be838
Mi1L4R <keagan.millar100@gmail.com>
2021-10-18 13:40:51 +0200

    - Added Vet list and updated controller and vets/Index view

--- pet-clinic-web/src/main/java/com/udemy/springpetclinic/Controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/udemy/springpetclinic/Controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.udemy.springpetclinic.Controllers;
 
+import Services.IVetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final IVetService vetService;
+
+    public VetController(IVetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
 
         return "vets/index";
     }
