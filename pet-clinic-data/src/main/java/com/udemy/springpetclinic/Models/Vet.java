41e4b3888fa54a099efdbe5727921169fddb0826
Mi1L4R <keagan.millar100@gmail.com>
2021-10-20 09:48:52 +0200

    Added Speciality class

--- pet-clinic-data/src/main/java/com/udemy/springpetclinic/Models/Vet.java
+++ pet-clinic-data/src/main/java/com/udemy/springpetclinic/Models/Vet.java
@@ -1,6 +1,11 @@
 package com.udemy.springpetclinic.Models;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<Speciality> specialities;
+
     public Vet() {
     }
 
@@ -13,5 +18,13 @@ public class Vet extends Person{
         super.setId(id);
         super.setFirstName(firstName);
         super.setLastName(lastName);
+    }
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }
